ee349a8697a8a52c773e87cf744021d7e0ea97e0
Howard M. Lewis Ship <hlship@gmail.com>
2022-08-08 16:41:42 -0700

    Setup build

---
+++ CHANGES.md
@@ -0,0 +1,3 @@
+# 0.1 -- 8 Aug 2022
+
+Initial release.
---
+++ build.clj
@@ -0,0 +1,31 @@
+;; clj -T:build <var>
+
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [net.lewisship.build :refer [requiring-invoke]]))
+
+(def lib 'io.github.hlship/cli-tools)
+(def version "0.1")
+
+(def jar-params {:project-name lib
+                 :version version})
+
+(defn clean
+  [_params]
+  (b/delete {:path "target"}))
+
+(defn jar
+  [_params]
+  (requiring-invoke net.lewisship.build.jar/create-jar jar-params))
+
+(defn deploy
+  [_params]
+  (clean nil)
+  (jar nil)
+  (requiring-invoke net.lewisship.build.jar/deploy-jar jar-params))
+
+(defn codox
+  [_params]
+  (requiring-invoke net.lewisship.build.codox/generate
+                    {:project-name lib
+                     :version version}))
