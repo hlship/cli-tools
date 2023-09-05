a8ae160501132393d435e741e15cc31ed77012ec
Howard M. Lewis Ship <hlship@gmail.com>
2023-09-05 13:45:17 -0700

    Add scripts to generate an app w/ 2000 commands

---
+++ scale-test/scale_test.clj
@@ -0,0 +1,55 @@
+(ns scale-test
+  (:require [clojure.string :as str]
+            [babashka.fs :as fs]
+            [selmer.parser :as selmer]))
+
+(def ns-count 100)
+(def command-count 20)
+
+(def root-dir "scale-test/uber")
+
+(def command-name-terms
+  (str/split "gnip frob update nerd spit snip echo gnop cluster system node module user role acl server service deployment"
+             #"\s+"))
+
+(defn gen-single-command-name
+  []
+  (let [terms (repeatedly 3 #(rand-nth command-name-terms))]
+    (str/join "-" terms)))
+
+(def command-names
+  (->> (repeatedly gen-single-command-name)
+       distinct))
+
+
+(defn render
+  [from to context-map]
+  (let [output      (selmer/render-file (str "templates/" from) context-map)
+        output-path (str root-dir "/" to)]
+    (println "Writing:" output-path)
+    (spit output-path output)))
+
+(defn write-ns
+  [i command-names]
+  (let [ns-name (str "commands.command-ns-" i)
+        ns-file (-> ns-name
+                    (str/replace "-" "_")
+                    (str/replace "." "/")
+                    )]
+    (render "command-ns.edn"
+            (str "src/" ns-file ".clj")
+            {:ns            ns-name
+             :command-names command-names})
+    ns-name))
+
+((defn setup
+   []
+   (fs/delete-tree root-dir)
+   (fs/create-dirs (str root-dir "/src/commands"))
+   (let [commands-per-ns (->> (partition command-count command-names)
+                              (take ns-count))
+         ns-names        (doall (map-indexed write-ns commands-per-ns))]
+     ;; TODO: Maybe can set this up to use the local workspace version?
+     (render "bb.edn" "bb.edn" {})
+     (render "app.edn" "app" {:ns-names ns-names}))
+   (fs/set-posix-file-permissions (str root-dir "/app") "rwxrwxrwx")))
