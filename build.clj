e6722a07ea8378c559bb43b4f81b4c5f2dc387e5
Howard M. Lewis Ship <hlship@gmail.com>
2025-09-05 09:30:00 -0700

    Pre for 0.16.0-alpha-7

--- build.clj
+++ build.clj
@@ -5,7 +5,7 @@
             [net.lewisship.build :as b]))
 
 (def lib 'io.github.hlship/cli-tools)
-(def version "0.16.0-alpha-6")
+(def version "0.16.0-alpha-7")
 
 (def jar-params {:project-name lib
                  :version version})
